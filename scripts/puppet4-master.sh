ab80f77843412899b60bac116448ac36ffd3e892
Simon Hoenscheid <simon@hoenscheid-itconsulting.com>
2016-02-05 14:01:46 +0100

    added scripts and build description for puppet 3/4 master VMs

---
+++ scripts/puppet4-master.sh
@@ -0,0 +1,21 @@
+set -e
+
+# Prepare puppetlabs repo
+CODENAME=`lsb_release -cs`
+
+wget http://apt.puppetlabs.com/puppetlabs-release-pc1-"$CODENAME".deb
+dpkg -i puppetlabs-release-pc1-"$CODENAME".deb
+apt-get update
+
+# Install puppet/facter
+apt-get install -y puppetserver
+rm -f puppetlabs-release-pc1-"$CODENAME".deb
+
+# Disable Puppet agent on systemd
+# PuppetLabs has the service disabled per default
+if which systemctl >/dev/null; then
+  systemctl stop puppet
+  systemctl disable puppet
+  systemctl start puppetserver
+  systemctl enable puppetserver
+fi
